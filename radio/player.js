00ea8f26506846652325da038a4a116824236d1a
Ari801123 <zdunicarijan07@gmail.com>
2026-06-28 17:57:49 +0200

    player.js

--- radio/player.js
+++ radio/player.js
@@ -15,10 +15,7 @@ const RADIO_URL = "https://streaming.tdiradio.com/lollipop.mp3";
 
 const player = createAudioPlayer();
 
-player.on("error", error => {
-  console.error("PLAYER ERROR:", error);
-});
-
+player.on("error", error => console.error("PLAYER ERROR:", error));
 player.on("stateChange", (oldState, newState) => {
   console.log(`Player state: ${oldState.status} -> ${newState.status}`);
 });
@@ -27,19 +24,24 @@ function createResource() {
   console.log("Pokrećem FFmpeg...");
 
   const ff = spawn(ffmpeg, [
+    "-hide_banner",
+    "-nostdin",
+    "-loglevel", "info",
+
     "-reconnect", "1",
+    "-reconnect_at_eof", "1",
     "-reconnect_streamed", "1",
-    "-reconnect_delay_max", "5",
+    "-reconnect_delay_max", "10",
 
     "-user_agent", "Mozilla/5.0",
     "-headers", "Icy-MetaData: 1\r\n",
 
-    "-i", RADIO_URL,
-
-    "-loglevel", "warning",
     "-analyzeduration", "1000000",
     "-probesize", "1000000",
 
+    "-i", RADIO_URL,
+
+    "-vn",
     "-f", "s16le",
     "-ar", "48000",
     "-ac", "2",
@@ -55,8 +57,8 @@ function createResource() {
     console.error("FFMPEG ERROR:", error);
   });
 
-  ff.on("close", code => {
-    console.log("FFMPEG CLOSED:", code);
+  ff.on("close", (code, signal) => {
+    console.log("FFMPEG CLOSED:", { code, signal });
   });
 
   return createAudioResource(ff.stdout, {
@@ -76,16 +78,16 @@ async function play(channel) {
   });
 
   await entersState(connection, VoiceConnectionStatus.Ready, 20000);
-
   console.log("Voice connection READY");
 
   connection.subscribe(player);
 
   player.removeAllListeners(AudioPlayerStatus.Idle);
-
   player.on(AudioPlayerStatus.Idle, () => {
-    console.log("Player idle, restartam stream...");
-    player.play(createResource());
+    console.log("Player idle, restartam stream za 5 sekundi...");
+    setTimeout(() => {
+      player.play(createResource());
+    }, 5000);
   });
 
   console.log("Pokrećem player...");
@@ -94,6 +96,4 @@ async function play(channel) {
   return connection;
 }
 
-module.exports = {
-  play
-};
+module.exports = { play };
